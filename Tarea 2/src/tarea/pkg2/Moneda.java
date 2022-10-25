cf50be8e82df91190f2395400f70c19db9c11a84
rcadiz2020 <rcadiz2020@udec.cl>
2022-10-25 16:40:31 -0300

    Cambios varios

--- Tarea 2/src/tarea/pkg2/Moneda.java
+++ Tarea 2/src/tarea/pkg2/Moneda.java
@@ -3,8 +3,22 @@ package tarea.pkg2;
 class Moneda {
 
     public Moneda(){
-
+        
+    }
+    public String getSerie(){
+        return this;
     }
 
 
+}
+
+class Moneda100 extends Moneda{
+    
+}
+
+class Moneda500 extends Moneda{
+}
+        
+class Moneda1000 extends Moneda{
+    
 }
