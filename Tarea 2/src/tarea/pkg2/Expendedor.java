4cc8fc24f0bc567968c3ca80b2791deba2cffab8
rcadiz2020 <rcadiz2020@udec.cl>
2022-10-26 19:56:55 -0300

    agregadas excepciones

--- Tarea 2/src/tarea/pkg2/Expendedor.java
+++ Tarea 2/src/tarea/pkg2/Expendedor.java
@@ -6,3 +6,21 @@ class Expendedor {
         Deposito d = new Deposito();
     }
 
+class PagoInsuficienteException extends Exception{
+    public PagoInsuficienteException(){
+        super("Dinero insuficiente");
+    }
+}
+
+class PagoIncorrectoException extends Exception{
+    public PagoIncorrectoException(){
+        super("Pago incorrecto");
+    }
+}
+
+class NoHayBebidaException extends Exception{
+    public NoHayBebidaException(){
+        super("Deposito de bebidas vacio");
+    }
+}
+
