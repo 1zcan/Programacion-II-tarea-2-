9b976718f1c2057b53a58f8893ab13082e3b72aa
rcadiz2020 <rcadiz2020@udec.cl>
2022-10-25 22:44:13 -0300

    Actualicacion clase Moneda y sus subclases

--- Tarea 2/src/tarea/pkg2/Moneda.java
+++ Tarea 2/src/tarea/pkg2/Moneda.java
@@ -8,17 +8,31 @@ class Moneda {
     public String getSerie(){
         return this;
     }
-
-
 }
 
 class Moneda100 extends Moneda{
-    
+    public Moneda100(){
+        
+    }
+    public int valor(){
+        return 100;
+    }
 }
 
 class Moneda500 extends Moneda{
+    public Moneda500(){
+        
+    }
+    public int valor(){
+        return 500;
+    }
 }
         
 class Moneda1000 extends Moneda{
-    
+    public Moneda1000(){
+        
+    }
+    public int valor (){
+        return 1000;
+    }
 }
