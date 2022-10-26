2e08811f2858af18a25fc2e71e940c7199cde3d6
rcadiz2020 <rcadiz2020@udec.cl>
2022-10-26 00:31:36 -0300

    Actualizacion Bebida y sus subclases

--- Tarea 2/src/tarea/pkg2/Bebida.java
+++ Tarea 2/src/tarea/pkg2/Bebida.java
@@ -3,12 +3,39 @@ package tarea.pkg2;
 
 
 class Bebida {
-    public Bebida(int numSerie){
-        
+    private String saborBebida;
+    private int precio;
+    public Bebida(int numSerie, int precio, String saborBebida){
+        this.saborBebida = saborBebida;
+        this.precio = precio;
+    }
+    public String getSabor(){
+        return saborBebida;
+    }
+    public int getPrecio(){
+        return precio;
     }
     public int getSerie(){
     }
     public abstract String beber(){
     }
-}   
+} 
+
+public class CocaCola extends Bebida{
+    public CocaCola (int precio){
+        super(precio);
+    }
+}
+
+public class Sprite extends Bebida{
+    public Sprite (int precio){
+        super(precio);
+    }
+}
+
+public class Fanta extends Bebida{
+    public Fanta (int precio){
+        super(precio);
+    }
+}
     
