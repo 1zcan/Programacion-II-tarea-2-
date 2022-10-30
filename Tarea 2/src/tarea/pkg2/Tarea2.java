5f86b2eaceda37d594c36662ae607fa0990b9f8f
rcadiz2020 <rcadiz2020@udec.cl>
2022-10-29 21:56:07 -0300

    monedas en main

--- Tarea 2/src/tarea/pkg2/Tarea2.java
+++ Tarea 2/src/tarea/pkg2/Tarea2.java
@@ -6,7 +6,11 @@ public class Tarea2 {
         
         Expendedor e = new Expendedor();
         Comprador p = new Comprador();
-        Moneda m = new Moneda();
+        Moneda100 moneda100 = new Moneda100();
+        Moneda500 moneda500 = new Moneda500();
+        Moneda1000 moneda1000 = new Moneda1000();
+        Moneda1500 moneda1500 = new Moneda1500();
+        
         
     }
 
